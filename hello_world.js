0c96990421dd787f2c60487b037368965d929652
Cedxx <cedmsm@hotmail.com>
2019-04-16 15:26:59 +0200

    modification du JS pour y ajouter les balise HTLM + Header

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,14 @@
-alert("Bonjour, le Monde!");
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bonjour le Monde!</title>
+</head>
+
+<body>
+
+    <script>
+        alert('Bonjour le Monde!');
+    </script>
+
+</body>
+</html>
